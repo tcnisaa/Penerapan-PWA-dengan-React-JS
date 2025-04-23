095725869f339c24d79b6c1dcf1cc82297be473e
nisa <tangguhchairunnisa04@gmail.com>
2025-04-23 15:39:23 +0700

    first commit

---
+++ public/service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "my-cache-v1";
+const urlsToCache = [
+  "/",            // Halaman utama
+  "/index.html",  // File HTML utama
+  "/static/js/bundle.js", // Bundel JS
+  "/static/css/main.css", // Bundel CSS
+  // Tambahkan file atau URL lain yang ingin kamu cache
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+// Pages
+import LandingPage from "./pages/LandingPages";
+
+
+// Components
+import Header from "./components/header";
+
+
+import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<LandingPage/>} />
+          </Routes>
+      </Router>
     </div>
   );
 }
 
+
 export default App;
+
